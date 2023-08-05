75ab7febeedf8ec1292596c617e7e0a6cd864a48
Magabriela <magalopeap@gmail.com>
2023-08-04 22:06:49 -0500

    update: var, let, print

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -6,3 +6,28 @@ var currentLoginAttempt = 0
 
 var x = 0.0, y = 0.0, z = 0.0
 
+var welcomeMessage : String
+
+welcomeMessage = "Hola, ¿qué tal?"
+
+print(welcomeMessage)
+
+var red, green, blue : Double
+
+//camelCase
+
+//snake_case
+
+//kebap-case
+
+let π = 3.141592
+var 🥶 = "Frozen"
+print(🥶)
+
+🥶 = "Frozen Two"
+print(🥶)
+
+print("hola \(π)", "El número de login actual es: \(currentLoginAttempt) de un total disponible de: \(maximunNumberOfLoginAttempts)")
+
+print("Hola", "Soy Maga", "No es mi nombre pero puedes llamarme así", separator: ", ", terminator: ".")
+
