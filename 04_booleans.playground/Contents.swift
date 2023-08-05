2926a526d1166537372623ef5f886885bad33c52
Magabriela <magalopeap@gmail.com>
2023-08-04 23:02:49 -0500

    feat: booleans

--- 04_booleans.playground/Contents.swift
+++ 04_booleans.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let orangesAreOrange = true
+let foodIsDelicius = false
+
+var isAged : Bool
+
+isAged = false
+
+if isAged {
+    print("Puedes entrar a la fiesta")
+}
+else {
+    print("Mejor ve a casa")
+}
+
+var age : UInt8 = 28
+
+if age >= 18 {
+    print("Puedes entrar a la fiesta")
+}
+else {
+    print("Mejor ve a casa")
+}
